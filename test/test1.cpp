d3e78bcba9a8725515afcdd1a11782a70189335c
AAnzel <alexein@pop-os.localdomain>
2019-05-10 16:27:40 +0200

    Ispravljanje greske i dokumentacije.

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,6 +1,6 @@
 int main () {
    int k = 42;
-   int a = k + k;	// a = 84
-   int d = (a + a) - (a - a);	// d = 84
-   return (d + a);	// 168
+   int a = k + k;
+   int d = (a + a) - (a - a);
+   return (d + a);
 }
